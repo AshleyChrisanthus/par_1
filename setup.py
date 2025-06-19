55c33d94b28e842bea9fa0b858dea85ee9c27ac0
Oneeba Ahsan <50304829+OneebaAhsan@users.noreply.github.com>
2025-06-19 17:52:21 +1000

    adding obstacle_manager

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setup(
             'hybrid_tennis_ball_detector = par_1.hybrid_tennis_ball_detector:main',  # detection from Ash, depth and dist from Aks
             'hybrid_tennis_ball_detector_no_transform = par_1.hybrid_tennis_ball_detector_no_transform:main',  # detection from Ash, depth and dist from Aks, no transformation
             'nav2_mission_planner_avoidance = par_1.nav2_mission_planner_avoidance:main',
+            'obstacle_manager = par_1.obstacle_manager:main',
         ],
     },
 )
